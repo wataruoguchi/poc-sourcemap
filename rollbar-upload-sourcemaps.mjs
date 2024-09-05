58aaa6d4ed476f5b51db77b13dffb2a947b33cc1
wataru <wataru.oguchi.0to1@gmail.com>
2024-09-05 12:04:03 -0700

    Remove the dependency

--- rollbar-upload-sourcemaps.mjs
+++ rollbar-upload-sourcemaps.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import "zx/globals";
 import { openAsBlob } from "node:fs";
+import { execSync } from "node:child_process";
 
 const hostName = process.env.APP_HOST;
 const accessToken = process.env.ROLLBAR_ACCESS_TOKEN_SERVER;
@@ -12,11 +12,18 @@ if (!hostName || !accessToken || !gitVersion) {
   process.exit(1);
 }
 
+const isDry = !(process.argv[2] && process.argv[2] == "--non-dry");
+
 // URL to your minified file without schema
 const minifiedUrlHost = hostName.replace(/https?:/, "");
 
 // Look up against gh-pages branch
-const jsFiles = (await $`find assets -name "*.js"`.text())
+const jsFiles = execSync(
+  gitVersion === "0"
+    ? `find dist/assets -name "*.js"`
+    : `find assets -name "*.js"`,
+)
+  .toString()
   .split("\n")
   .filter((file) => file.length > 0);
 
@@ -33,6 +40,14 @@ async function uploadSourcemap(jsFile) {
   form.append("version", gitVersion);
   form.append("minified_url", `${minifiedUrlHost}${jsFile}`);
   form.append("source_map", blob);
+
+  if (isDry) {
+    console.log("Dry run: would upload sourcemap for", jsFile, {
+      gitVersion,
+      minified_url: `${minifiedUrlHost}${jsFile}`,
+    });
+    return;
+  }
 
   await fetch("https://api.rollbar.com/api/1/sourcemap", {
     method: "POST",
