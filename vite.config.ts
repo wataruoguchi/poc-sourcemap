702e8b59ac700e9c3b2766149e00e6330807e71b
wataru <wataru.oguchi.0to1@gmail.com>
2024-09-05 13:47:15 -0700

    vite-plugin-css-injected-by-js messes up the sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { execSync } from "node:child_process";
 import { defineConfig } from "vite";
-import cssInjectedByJSPlugin from "vite-plugin-css-injected-by-js";
 
 const gitVersion = execSync("git rev-parse --short HEAD").toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJSPlugin()],
+  plugins: [react()],
   define: {
     "import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN_CLIENT": `"${process.env.ROLLBAR_ACCESS_TOKEN_CLIENT ?? ""}"`,
     "import.meta.env.VITE_GIT_VERSION": `"${gitVersion}"`,
